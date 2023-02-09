47c3e7739e5825df05b0df7750f92c1005e27871
jinach0i <ginach0irk@gmail.com>
2023-02-09 17:21:33 +0900

    marked #footer up

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,5 +1,5 @@
 let myFullpage = new fullpage('#container', {
-    anchors:['main', 'about', 'value','news','recruitment'],
+    anchors:['Slide1', 'Slide2', 'Slide3','Slide4','Slide5'],
     navigation: true,
     navigationPosition: 'left',
     navigationTooltips: ['메인', '캠트로닉스','가치','뉴스','인사제도'],
--- js/slick1.js
+++ js/slick1.js
@@ -21,4 +21,40 @@ $(function () {
         speed: 1000,
         pauseOnHover: false,
     });
+
+    let preBtn=$('.prev'),
+    nextBtn=$('.next');
+    $('.news-slider').on('init reInit afterChange', function (event,slick,cur) {
+        let i=(cur?cur:0)+1;
+        $('.pagination').text(i+'  /  '+slick.slideCount)
+    });
+    $('.icon').on('click', function () {
+        if ($(this).attr('class') == 'fa-solid' && 'fa-pause'){
+            $('.news-slider').slick('slickPause');
+            $(this).removeClass('fa-pause');
+            $(this).addClass('fa-play');
+         } else {
+             $(this).removeClass('fa-play');
+           $('.news-slider').slick('slickPlay'); 
+           $(this).addClass('fa-pause');
+         }  
+    });
+    $('.news-slider').slick({
+        accessibility: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: true,
+        prevArrow:preBtn,
+        nextArrow:nextBtn,
+        dots: false,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        pauseOnHover: false,
+        draggable: true,
+    });
+
+
+    
 })
