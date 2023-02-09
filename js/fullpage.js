c7d7f4a914c346f4c20fe98b432bf8af828c6c96
jinach0i <ginach0irk@gmail.com>
2023-02-09 11:09:33 +0900

    finish #value markup

--- js/fullpage.js
+++ js/fullpage.js
@@ -3,6 +3,7 @@ let myFullpage = new fullpage('#container', {
     navigation: true,
     navigationPosition: 'left',
     navigationTooltips: ['메인', '캠트로닉스','가치','뉴스','인사제도'],
+    slidesNavigation: true,
     css3:true,
     easing: 'easeInOutCubic',
     fitToSection: true,
